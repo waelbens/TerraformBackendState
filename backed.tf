e55560fc06ce6f24b238e6e5e65b5740f5e498f7
waelbens <wael.ben.salah@gmail.com>
2018-11-19 21:06:22 +0100

    Add State lock option

--- backed.tf
+++ backed.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
+    encrypt = true
     bucket = "terraform-backend-state-wael"
     key    = "state"
+    dynamodb_table = "terraform-state-lock-dynamo"
     region = "us-east-2"
   }
 }
